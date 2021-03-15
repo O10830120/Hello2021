7a7cf5e21e478547813ef55951d65d416bf740b9
O1083012 <o1083012@gm.pu.edu.tw>
2021-03-15 11:32:12 +0800

    第三次 commit（加入dev分支程式碼）

--- app/src/main/java/pu/edu/tw/hello2021/MainActivity.kt
+++ app/src/main/java/pu/edu/tw/hello2021/MainActivity.kt
@@ -9,7 +9,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello o10830120"
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+        TestDEV()
+    }
+
+    fun TestDEV(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Dev分支"
     }
 }
